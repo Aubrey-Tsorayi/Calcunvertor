60a48e506bc7f03a8e9858c78aee101fc125997f
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2024-01-24 14:58:39 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/calcunvertor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calcunvertor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      useMaterial3: true,
+    ),
+  ));
+}
+
+// creating calculator page
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
